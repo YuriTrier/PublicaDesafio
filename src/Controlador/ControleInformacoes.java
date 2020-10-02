4d01e1fb9cc96e0b79ae1a54afdf5ffc6b8678bc
Yuri Trierveiler <yuri.trier@gmail.com>
2020-10-01 23:19:23 -0300

    Ajuste do código, inclusão de informações na tabela. Salvando em arquivo dados

--- src/Controlador/ControleInformacoes.java
+++ src/Controlador/ControleInformacoes.java
@@ -7,17 +7,24 @@ package Controlador;
 
 import java.util.ArrayList;
 import Modelo.Jogador;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  *
  * @author Yuri
  */
 public class ControleInformacoes {
     
-    private ArrayList<Jogador> jogos;
+    public ArrayList<Jogador> jogos;
     private int pontuacaoMaxima;
     private int pontuacaoMinima;
     private int qtdQuebrouMax;
     private int qtdQuebrouMin;
+    private int idUltimoJogo;
     
 public ControleInformacoes(){ 
     jogos = new ArrayList<Jogador>();
@@ -25,6 +32,7 @@ public ControleInformacoes(){
     pontuacaoMinima = 99999;
     qtdQuebrouMax = 0;
     qtdQuebrouMin = 0;
+    idUltimoJogo =0;
     
 }   
     
@@ -44,8 +52,40 @@ public void novoJogo(Jogador jogo){
     jogo.setPontuacaoMinima(pontuacaoMinima);
     jogo.setQtdRecordeMax(qtdQuebrouMax);
     jogo.setQtdRecordeMin(qtdQuebrouMin);
+    jogo.setJogo(++idUltimoJogo);
     }
  
-    
+ 
+    public void gravaInformacoes(String caminho) throws IOException{
+        FileWriter writer = new FileWriter(caminho);
+        
+        BufferedWriter salvar = new BufferedWriter(writer);
+        for (Jogador jogo : jogos) {
+            salvar.write(jogo.getJogo()+";"+jogo.getPlacar()+";"+jogo.getQtdRecordeMax()+";"+jogo.getQtdRecordeMin()+";"+jogo.getPontuacaoMaxima()+";"+jogo.getPontuacaoMinima()+";");
+            salvar.newLine();
+        }
+        salvar.close(); 
+    }
+
+    public void carregarArquivo(String caminho) throws FileNotFoundException, IOException{
+    BufferedReader carregar = new BufferedReader(new FileReader(caminho));
+        
+    String linha = carregar.readLine();
+        
+    while (linha instanceof String){
+        String [] valores = linha.split(";");
+        Jogador jogo = new Jogador("Maria", 0, 0, 0, 0);
+        jogo.setPlacar(Integer.parseInt(valores[1]));
+        novoJogo(jogo);
+        linha = carregar.readLine();
+        }
+        carregar.close();
+    }
+    public Jogador retornarUltimoJogo(){
+        if (!jogos.isEmpty()){
+            return jogos.get(jogos.size()-1);
+        }
+        return null;
+    }
 }
 
